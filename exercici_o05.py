883b00dc0c23fe7a665d0863cd1002577e912bf9
LilieHwang <00001130429489sp@al.educacao.sp.gov.br>
2025-05-13 11:52:20 -0300

    Informe que criamos um codigo para cadastrar alunos

--- exercici_o05.py
+++ exercici_o05.py
@@ -8,11 +8,13 @@ def obter_dados_aluno():
     serie_aluno = input("informe a serie do aluno: ")
     nota01_aluno = int(input("informe a primeira nota do aluno: "))
     nota02_aluno = int(input("informe a segunda nota do aluno: "))
-    nota033_aluno = int(input("informe a terceira nota do aluno: "))
+    nota03_aluno = int(input("informe a terceira nota do aluno: "))
 
-def cadastrar_aluno(nome, email, serie, nota_01=0, nota_02=0, nota_03=0):
+    return cadastrar_aluno(nome_aluno, email_aluno, serie_aluno, nota01_aluno, nota02_aluno, nota03_aluno)
 
-    nota = [nota_01, nota_02, nota_03]
+def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
+
+    nota = [nota01, nota02, nota03]
 
     aluno = {
        "nome": nome, 
@@ -26,6 +28,27 @@ def cadastrar_aluno(nome, email, serie, nota_01=0, nota_02=0, nota_03=0):
     return aluno
 
 def mostrar_dados_alunos(dados_alunos):
-    return print(dados_alunos)
+    for aluno in dados_alunos:
+        print(f"Nome do Aluno: {aluno["nome"]} | Email do Aluno: {aluno["email"]}) | Série do aluno: {aluno["serie"]} | Notas do aluno: {aluno["notas"]} | Média do aluno: {aluno["media"]}")
 
-mostrar_dados_alunos(alunos)
+    return 
+
+def iniciar_sistema():
+    while True: 
+        print("="*80)
+        print("Opção 1 => mostrar alunos cadastrados")
+        print("Opção 2 => Cadastrar alunos.")
+        print("Opção 3 => Sair do sistema.")
+        print("="*80)
+
+        opcao = input("Escolha uma das opcões acima: ")
+
+        if opcao == "1":
+            mostrar_dados_alunos(alunos)
+        elif opcao == "2":
+            obter_dados_aluno()
+        else: 
+            print("sistema finalizado.")
+            break
+            
+iniciar_sistema()
